fafa83be8b925d5f50aa447af85dcb422662cac2
parthpsp1 <pspparth1998@gmail.com>
2025-03-13 17:14:25 +0530

    updated

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -16,61 +16,76 @@ class AboutScreen extends StatelessWidget {
             fontWeight: FontWeight.w500,
           ),
         ),
+        titleSpacing: 0,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Text(
-              "Credits",
-              style: TextStyle(
-                color: Color.fromARGB(255, 17, 27, 47),
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  "Credits",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 17, 27, 47),
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Remix.lightbulb_line),
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text(
+                  "Varun Pardeshi",
+                  style: TextStyle(
+                    color: Color.fromRGBO(17, 27, 47, 1),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                subtitle: Text("Ideation"),
+                trailing: Icon(
+                  Remix.linkedin_fill,
+                  color: Colors.blue,
+                ),
+                iconColor: Colors.blue,
+                splashColor: Colors.blue[100],
+                onTap: () => openLinkedInProfileVarun(),
+              ),
+              ListTile(
+                leading: Icon(Remix.code_s_slash_line),
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text(
+                  "Parth Pujari",
+                  style: TextStyle(
+                    color: Color.fromRGBO(17, 27, 47, 1),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                subtitle: Text("Development"),
+                trailing: Icon(
+                  Remix.linkedin_fill,
+                  color: Colors.blue,
+                ),
+                iconColor: Colors.blue,
+                splashColor: Colors.blue[100],
+                onTap: () => openLinkedInProfileParth(),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () => showLicensePage(context: context),
+                child: Text("Licenses"),
               ),
             ),
           ),
-          ListTile(
-            leading: Icon(Remix.lightbulb_line),
-            titleAlignment: ListTileTitleAlignment.center,
-            title: Text(
-              "Varun Pardeshi",
-              style: TextStyle(
-                color: Color.fromRGBO(17, 27, 47, 1),
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            subtitle: Text("Ideation"),
-            trailing: Icon(
-              Remix.linkedin_fill,
-              color: Colors.blue,
-            ),
-            iconColor: Colors.blue,
-            splashColor: Colors.blue[100],
-            onTap: () => openLinkedInProfileVarun(),
-          ),
-          ListTile(
-            leading: Icon(Remix.code_s_slash_line),
-            titleAlignment: ListTileTitleAlignment.center,
-            title: Text(
-              "Parth Pujari",
-              style: TextStyle(
-                color: Color.fromRGBO(17, 27, 47, 1),
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            subtitle: Text("Development"),
-            trailing: Icon(
-              Remix.linkedin_fill,
-              color: Colors.blue,
-            ),
-            iconColor: Colors.blue,
-            splashColor: Colors.blue[100],
-            onTap: () => openLinkedInProfileParth(),
-          )
         ],
       ),
     );
